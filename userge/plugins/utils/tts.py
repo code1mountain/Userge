1db2ef9799935a51e400581e0a44338868c20fd4
Jannik <32801117+code1mountain@users.noreply.github.com>
2020-05-08 07:19:18 +0200

    Update tts.py

--- userge/plugins/utils/tts.py
+++ userge/plugins/utils/tts.py
@@ -64,11 +64,11 @@ def generate_voice(text, file_out, voice=voices[5]):
     for t in split_string(text):
         
         url = getAudioUrl(t, voice)
-        r = requests.get(url, timeout=10000)
+        r = requests.get(url)
         with open(file_out, "wb") as f:
             f.write(r.content)
         n = 0
-        while os.path.getsize(file_out) < 2000 and n > 5:
+        while os.path.getsize(file_out) < 2000 and n < 5:
             n += 1
             url = getAudioUrl(text, voice)
             r = requests.get(url)
